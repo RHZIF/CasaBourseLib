0bc521a24de9b110191db42f332ec9e60e6db193
ahennamed <73837322+ahennamed@users.noreply.github.com>
2021-01-03 17:03:58 +0100

    get aspx file

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import requests
-Casablanca Bourse
+
+def bourse_maroc(company):
+    s = requests.Session()
+    response = s.get(f'http://www.casablanca-bourse.com/bourseweb/Societe-Cote.aspx?codeValeur={str(company)}')
+    cookies = response.cookies.get_dict()
+
+    source = s.get('http://www.casablanca-bourse.com/BourseWeb/Telechargement/Telechargement-Histo-Valeur.aspx', cookies=cookies)
+    if source.status_code == 200:
+        with open(f"{str(company)}.aspx", 'wb') as f:
+            f.write(source.content)
+
